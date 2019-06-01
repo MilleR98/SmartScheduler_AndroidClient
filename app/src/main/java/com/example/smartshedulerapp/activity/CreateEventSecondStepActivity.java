c4ebfc3f991c43ada1f85a505a1a3ef037d38c92
Oleg Melnyk <mr.oleg.melnyk@gmail.com>
2019-06-01 18:59:45 +0300

    Added location input by google places

--- app/src/main/java/com/example/smartshedulerapp/activity/CreateEventSecondStepActivity.java
+++ app/src/main/java/com/example/smartshedulerapp/activity/CreateEventSecondStepActivity.java
@@ -1,9 +1,11 @@
 package com.example.smartshedulerapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,6 +21,13 @@ import com.example.smartshedulerapp.model.EventDTO;
 import com.example.smartshedulerapp.model.EventLocation;
 import com.example.smartshedulerapp.model.EventMemberDTO;
 import com.example.smartshedulerapp.model.type.EventMemberPermission;
+import com.example.smartshedulerapp.util.PlacesFieldSelector;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AddressComponent;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
@@ -29,20 +38,20 @@ import retrofit2.Response;
 
 public class CreateEventSecondStepActivity extends AppCompatActivity implements InviteMemberDialog.InviteMemberDialogListener {
 
+  private static final int PLACE_PICKER_REQUEST = 1;
+
   @Inject
   EventApiService eventApiService;
 
   @BindView(R.id.membersListView)
   ListView membersListView;
-  @BindView(R.id.inputCountry)
-  EditText inputCountry;
-  @BindView(R.id.inputCity)
-  EditText inputCity;
-  @BindView(R.id.inputStreet)
-  EditText inputStreet;
+  @BindView(R.id.locationInput)
+  EditText inputLocation;
 
   private List<EventMemberDTO> memberList = new ArrayList<>();
   private MemberAdapter memberAdapter;
+  private PlacesFieldSelector fieldSelector;
+  private EventLocation eventLocation;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +65,47 @@ public class CreateEventSecondStepActivity extends AppCompatActivity implements 
 
     memberAdapter = new MemberAdapter(getApplicationContext(), memberList);
     membersListView.setAdapter(memberAdapter);
+
+    Places.initialize(getApplicationContext(), "AIzaSyAE_63RwAK2MUch6KCCiQrRoJDfNvFtE6Q");
+
+    fieldSelector = new PlacesFieldSelector();
+
+    eventLocation = new EventLocation();
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
+    if (resultCode == AutocompleteActivity.RESULT_OK) {
+
+      Place place = Autocomplete.getPlaceFromIntent(intent);
+
+      inputLocation.setText(place.getAddress());
+
+      List<AddressComponent> addressComponents = place
+          .getAddressComponents()
+          .asList();
+
+      addressComponents.forEach(this::setLocationFields);
+
+    }
+
+    super.onActivityResult(requestCode, resultCode, intent);
+  }
+
+  private void setLocationFields(AddressComponent addressComponent) {
+    if (addressComponent.getTypes().contains("street_number")) {
+
+      eventLocation.setBuildingNumber(addressComponent.getShortName());
+    } else if (addressComponent.getTypes().contains("route")) {
+
+      eventLocation.setStreet(addressComponent.getShortName());
+    } else if (addressComponent.getTypes().contains("locality")) {
+
+      eventLocation.setCity(addressComponent.getShortName());
+    } else if (addressComponent.getTypes().contains("country")) {
+
+      eventLocation.setCountry(addressComponent.getShortName());
+    }
   }
 
 
@@ -88,14 +138,18 @@ public class CreateEventSecondStepActivity extends AppCompatActivity implements 
     finish();
   }
 
+  @OnClick(R.id.locationInput)
+  public void locationInputOnClick() {
+
+    Intent autocompleteIntent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fieldSelector.getAllFields())
+        .build(this.getApplicationContext());
+
+    startActivityForResult(autocompleteIntent, PLACE_PICKER_REQUEST);
+  }
+
   @OnClick(R.id.submitEventCreating)
   public void createEventSubmitting() {
     EventDTO eventDTO = (EventDTO) getIntent().getSerializableExtra("eventDTO");
-
-    EventLocation eventLocation = new EventLocation();
-    eventLocation.setCity(inputCity.getText().toString());
-    eventLocation.setCountry(inputCountry.getText().toString());
-    eventLocation.setStreet(inputStreet.getText().toString());
 
     eventDTO.setEventLocation(eventLocation);
     eventDTO.setCurrentUserPermission(EventMemberPermission.OWNER);
